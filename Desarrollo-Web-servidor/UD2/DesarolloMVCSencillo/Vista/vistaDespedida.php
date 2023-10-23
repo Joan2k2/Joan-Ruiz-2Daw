f1bd8a208bc963229c2af84554861adb5bccf655
Joan2k2 <joanruizmartinez@gmail.com>
2023-10-23 12:44:35 +0200

    subo cosas

--- Desarrollo-Web-servidor/UD2/DesarolloMVCSencillo/Vista/vistaDespedida.php
+++ Desarrollo-Web-servidor/UD2/DesarolloMVCSencillo/Vista/vistaDespedida.php
@@ -1,7 +1,7 @@
 <?php
 
 class VistaDespedida{
-
+//muestra una despedida
     function despedirse($tiempo){
         
         echo"<h1>Adios navegante son las {$tiempo}</h1>";
--- Desarrollo-Web-servidor/UD2/DesarolloMVCSencillo/Vista/vistaSaludar.php
+++ Desarrollo-Web-servidor/UD2/DesarolloMVCSencillo/Vista/vistaSaludar.php
@@ -1,7 +1,7 @@
 <?php
 
 class VistaSaludar{
-
+//saluda
     function saludar($tiempo){
         
         echo"<h1>Saludos navegante son las {$tiempo}</h1>";
