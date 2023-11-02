6332943931ed9b29a25e4374b358905104628eb0
Joan2k2 <joanruizmartinez@gmail.com>
2023-11-02 23:59:13 +0100

    a

--- Desarrollo web entorno cliente/TAREA 5/Ejercicios 1 a 4/ejercicio4.js
+++ Desarrollo web entorno cliente/TAREA 5/Ejercicios 1 a 4/ejercicio4.js
@@ -1,25 +1,35 @@
 let imgs= document.getElementsByTagName("img");
 
-
+//si pongo un imagen.hidden en el ejercicio de abajo funciona
 imgs = Array.from(imgs).map((imagen, index) => {
     imagen.addEventListener('click', () => {
+        //console.log(imagen);
         ocultarFotos();
+        
         console.log(`Se hizo clic en la imagen ${index + 1}`);
         imagen.hidden=false;
+        imagen.addEventListener('click', () => {
+            //console.log(imagen);
+            mostrarFotos();
+            
+            console.log(`Se hizo clic en la imagen ${index + 1}`);
+            
+        });
     });
 });
 
 
 function ocultarFotos() {
-    
+    let imgs= document.getElementsByTagName("img");
     imgs = Array.from(imgs).map((img,index) => {
-        img[index].hidden = true;
+        console.log(img);
+        img.hidden = true;
         
     });
 }
 
 function mostrarFotos() {
-
+    let imgs= document.getElementsByTagName("img");
     imgs = Array.from(imgs).map(img => {
         img.hidden = false;
         return img;
