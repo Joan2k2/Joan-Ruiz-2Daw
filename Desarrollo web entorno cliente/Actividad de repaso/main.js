8bd18745efcde076147e48297fa67733a462f67c
Joan2k2 <joanruizmartinez@gmail.com>
2023-11-17 20:02:18 +0100

    continuo repaso paco

--- Desarrollo web entorno cliente/Actividad de repaso/main.js
+++ Desarrollo web entorno cliente/Actividad de repaso/main.js
@@ -1,31 +1,73 @@
 let pr = document.getElementById("keyboard-cont").innerHTML;
-let palabra=[];
+
 borrarTeclado();
-teclado();
+let passw = "";
+//localStorage.clear();
+
+document.getElementsByTagName("h1")[0].addEventListener("click",()=>{
+  let dataObtained = JSON.parse(localStorage.getItem('words')) || [];
+
+  if(dataObtained.length>0){
+
+    dataObtained.forEach((element,index) => {
+      console.log(element.palabra);
+      
+    });
+  }
+
+});
+
+
+document.addEventListener("keypress", (event) => {
+
+
+  passw += event.key;
+  console.log(passw);
+  if (passw.length > 11) {
+    passw = passw.slice(1);
+  }
+  if (passw === "abracadabra") {
+    passw = "";
+    teclado();
+  } else if (passw == "patadecabra") {
+    passw = "";
+    borrarTeclado();
+  }
+
+});
+
+//console.log(passw);
 
 function teclado() {
+  let palabra="" ;
   var keyboardCont = document.getElementById("keyboard-cont");
   keyboardCont.innerHTML = pr;
-  
+
   let tecla = document.getElementsByClassName("keyboard-button");
   tecla = Array.from(tecla);
   for (let i = 0; i < tecla.length; i++) {
-    console.log("BIAU");
     let p = tecla[i].innerHTML;
 
     if (p == "Del") {
       tecla[i].addEventListener("click", () => {
-        palabra.pop();
-        console.log(palabra);
+        palabra = palabra.slice(0, -1);
+        //console.log(palabra);
       });
     } else if (p == "Enter") {
       tecla[i].addEventListener("click", () => {
-        console.log("PPPPPPPPPP");
+        event.preventDefault();
+        let dataSaved = JSON.parse(localStorage.getItem('words')) || [];
+       
+          
+        console.log(palabra);
+        dataSaved.push({ palabra});
+        palabra="";
+        localStorage.setItem('words', JSON.stringify(dataSaved));
       });
     } else {
       tecla[i].addEventListener("click", () => {
-        palabra.push(p);
-        console.log(palabra);
+        palabra+=p;
+        //console.log(palabra);
       });
     }
   }
