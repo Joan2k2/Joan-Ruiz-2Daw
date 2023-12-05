40b3783ccd58dd70bf8dee42aae1476a96819543
Joan2k2 <joanruizmartinez@gmail.com>
2023-12-05 11:50:37 +0100

    lo de jorge

--- Desarrollo-Web-servidor/UD4/Ejercicio1-Sintocar/src/Core/Dispatcher.php
+++ Desarrollo-Web-servidor/UD4/Ejercicio1-Sintocar/src/Core/Dispatcher.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Core;
-
+error_reporting(E_ALL);
+ini_set("display_errors",1);
 use App\Core\Interfaces\IRequest;
 use App\Core\Interfaces\IRoutes;
 
@@ -19,7 +20,8 @@ class Dispatcher
     {
       //  echo $this->currentRequest->getRoute();
         if (isset($this->routeList[$this->currentRequest->getRoute()])) {
-            $controllerClass = "App\\Controllers\\" . $this->routeList[$this->currentRequest->getRoute()]["controller"];
+            $controllerClass = "App\\Repository\\" . $this->routeList[$this->currentRequest->getRoute()]["controller"];
+            //le tengo que pasar un objeto al controller que sea abstracto?
             $controller =  new $controllerClass;
             $action = $this->routeList[$this->currentRequest->getRoute()]["action"];
             $controller->$action(...$this->currentRequest->getParams());
--- Desarrollo-Web-servidor/UD4/Ejercicio1-Sintocar/src/Repository/TareaRepository.php
+++ Desarrollo-Web-servidor/UD4/Ejercicio1-Sintocar/src/Repository/TareaRepository.php
@@ -1,14 +1,74 @@
 <?php
 
 namespace App\Repository;
-
+error_reporting(E_ALL);
+ini_set("display_errors",1);
 use App\Entity\Tarea;
 use App\Core\EntityManager;
 use Doctrine\ORM\EntityRepository;
+use App\Core\AbstractController;
 
-class TareaRepository extends EntityRepository
+
+class TareaRepository extends EntityRepository 
 {
+    private $abs;
+
+    public function __construct(AbstractController $abs)
+    {
+        $this->abs = $abs;
+    }
+
+public function add()
+{
+   
+   $em = (new EntityManager())->get();
+   $nuevo = new Tarea();
+   $nuevo->setTitulo("nuevaTarea");
+   $nuevo->setFecha_creacion(new \DateTime('2021-10-24'));
+   $em->persist($nuevo);
+   echo("he estado en add");
+   $em->flush();
+}
+public function listado($page = null)
+{
+   
+   $em = (new EntityManager())->get();
+   echo"1";
+   $usersRepository = $em->getRepository(Tarea::class);
+   echo"2";
+   $this->abs->render("list.html.twig", [
+      "resultados" => $usersRepository->findAll()
+   ]);
+}
+
+   // public function update($id)
+   // {
+   //    echo("he estado en update");
+   //    $em = (new EntityManager())->get();
+
+   //    $tareaRepository = $this->repo;
+   //    $tarea = $tareaRepository->find($id);
+
+   //    $tarea->setTitulo("Pepe");
+   //    $tarea->setFecha_creacion(new \DateTime('2021-10-22'));
+
+   //    $em->persist($tarea);
+      
+   //    $em->flush();
+   // }
+   // public function del($id)
+   // {
+   //    echo("he estado en delete");
+   //    $em = (new EntityManager())->get();
+   //    $tareaRepository = $this->repo;
+   //    $tarea = $tareaRepository->find($id);
+   //    if ($tarea) $em->remove($tarea);
+      
+   //    $em->flush();
+   // }
 
 }
+
+
 
 
