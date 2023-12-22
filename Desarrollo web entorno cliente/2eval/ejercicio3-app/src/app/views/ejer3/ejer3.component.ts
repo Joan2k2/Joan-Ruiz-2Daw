9f3cf68efdb437bf1040661f6a2b6adf4b03adff
Joan2k2 <joanruizmartinez@gmail.com>
2023-12-22 12:02:02 +0100

    a

---
+++ Desarrollo web entorno cliente/2eval/ejercicio3-app/src/app/views/ejer3/ejer3.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ejer3',
+  standalone: true,
+  imports: [],
+  templateUrl: './ejer3.component.html',
+  styleUrl: './ejer3.component.css'
+})
+export class Ejer3Component {
+
+}
