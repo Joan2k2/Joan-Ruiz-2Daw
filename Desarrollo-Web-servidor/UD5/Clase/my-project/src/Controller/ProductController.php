e65a8873553af7ad8e8e730d55fa4cd35fb7626f
Joan2k2 <joanruizmartinez@gmail.com>
2024-01-11 13:39:21 +0100

    subo jorge y paco

--- Desarrollo-Web-servidor/UD5/Clase/my-project/src/Controller/ProductController.php
+++ Desarrollo-Web-servidor/UD5/Clase/my-project/src/Controller/ProductController.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace App\Controller;
+use App\Core\Request;
 use App\Entity\Product;
+use App\Form\ProductType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,6 +11,14 @@ use Doctrine\ORM\EntityManagerInterface;
 
 class ProductController extends AbstractController
 {
+
+    	private $em;
+        public function __construct(EntityManagerInterface $em){
+
+            $this->em=$em;
+
+        }
+
     #[Route('/product', name: 'app_product')]
     public function index(): Response
     {
@@ -21,9 +31,9 @@ class ProductController extends AbstractController
     public function createProduct(EntityManagerInterface $entityManager): Response
     {
         $product = new Product();
-        $product->setName('Keyboard');
-        $product->setPrice(1999);
-        $product->setDescription('Ergonomic and stylish!');
+        $product->setName('Davit');
+        $product->setPrice(0);
+        $product->setDescription('Im free');
 
         // tell Doctrine you want to (eventually) save the Product (no queries yet)
         $entityManager->persist($product);
@@ -34,5 +44,77 @@ class ProductController extends AbstractController
         return new Response('Saved new product with id '.$product->getId());
     }
 
+    #[Route('/product/{id}', name: 'product_show')]
+    public function show(EntityManagerInterface $entityManager, int $id): Response
+    {
+        $product = $entityManager->getRepository(Product::class)->find($id);
+
+        if (!$product) {
+            return new Response('No hay un producto con esta id '. $id);
+        }
+
+        return new Response('Check out this great product: '.$product->getName());
+
+        // or render a template
+        // in the template, print things with {{ product.name }}
+        // return $this->render('product/show.html.twig', ['product' => $product]);
+    }
+
+    #[Route('/product/edit/{id}', name: 'product_edit')]
+    public function update(EntityManagerInterface $entityManager, int $id): Response
+    {
+        $product = $entityManager->getRepository(Product::class)->find($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException(
+                'No product found for id '.$id
+            );
+        }
+
+        $product->setName('Demonio pelirrojo!');
+        $entityManager->flush();
+
+        return $this->redirectToRoute('product_show', [
+            'id' => $product->getId()
+        ]);
+    }
+
+    #[Route('/product/delete/{id}', name: 'product_delete')]
+    public function delete(EntityManagerInterface $entityManager, int $id): Response
+    {
+        $product = $entityManager->getRepository(Product::class)->find($id);
+
+        if (!$product) {
+            return new Response('No hay un producto con esta id delete '. $id);
+        }
+        $entityManager->remove($product);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('product_show', [
+            'id' => $id
+        ]);
+    }
+
+
+    #[Route('/', name: 'app_home')]
+    public function home(Request $request): Response
+    {
+
+        $product= new Product();
+        $form=$this->createForm(ProductType::class,$product);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+
+            $this->em->persist($product);
+            $this->em->flush();
+            return $this->redirectToRoute('app_Product');
+
+        }
+
+        return $this->render('base.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
 
 }
---
+++ Desarrollo-web-entorno-cliente/2eval/Ejercicio4/src/app/views/fishes/fishes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fishes',
+  standalone: true,
+  imports: [],
+  templateUrl: './fishes.component.html',
+  styleUrl: './fishes.component.css'
+})
+export class FishesComponent {
+
+}
